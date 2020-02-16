1e5a82e59d64b2b468e38a55ae0f33897c973c3a
sysysysy242 <45331186+sysysysy242@users.noreply.github.com>
2020-02-16 14:43:54 +0100

    Update config.js

--- .gitignore/config.js
+++ .gitignore/config.js
@@ -2,7 +2,7 @@ module.exports = {
     // The token of the bot
 
     // The ID of the channel in which the users will be able to ask for their fake classic nitro
-    genClassic: "678371101864230933",
+    genClassic: "678597310380376065",
     // The ID of the channel in which the users will be able to ask for their fake games nitro
     genGames: "678371126933454880",
     // The prefix of the bot
